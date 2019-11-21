5f0a34d2afb4c8914edd469fe70900f867a37b99
Jadhiel Vélez <jadhielv@gmail.com>
2019-11-20 21:50:42 -0400

    .gitignore is now working

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ class App extends Component {
                             ))}
                         </ul>
                     </section>
+                    <hr/>
                 </div>
             </div>
         )
