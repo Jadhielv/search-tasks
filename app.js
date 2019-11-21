8d28fa23ae0212f245f2bfff2e81d53e1810ba54
Jadhiel Vélez <jadhielv@gmail.com>
2019-11-20 21:16:00 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import 'bulma/bulma';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: [
+                "Go to the store",
+                "Wash the dishes",
+                "Learn som code"
+            ]
+        }
+    }
+    render() {
+        return (
+            <div className="content">
+                <div className="container">
+                    <section className="section">
+                        <ul>
+                            {this.state.list.map(item => (
+                                <li key={item}>{item}</li>
+                            ))}
+                        </ul>
+                    </section>
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
