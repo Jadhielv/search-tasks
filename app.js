570c2d5aa26c77ec89d4fab80d9f6e597d3f20d0
Jadhiel Vélez <jadhielv@gmail.com>
2019-11-20 22:23:57 -0400

    add functionality to add item

--- app.js
+++ app.js
@@ -12,19 +12,53 @@ class App extends Component {
                 "Learn som code"
             ]
         }
+
+        this.addItem = this.addItem.bind(this);
     }
+
+    addItem(e) {
+        e.preventDefault();
+
+        let list = this.state.list;
+        const newItem = document.getElementById("addInput");
+        const form = document.getElementById("addItemForm");
+
+        if (newItem.value != "") {
+            list.push(newItem.value);
+
+            this.setState({
+                list: list
+            });
+
+            newItem.classList.remove("is-danger");
+            form.reset();
+        } else {
+            newItem.classList.add("is-danger");
+        }
+    }
+
     render() {
         return (
             <div className="content">
                 <div className="container">
                     <section className="section">
+                        <label className="has-text-weight-bold">List of Tasks:</label>
                         <ul>
                             {this.state.list.map(item => (
                                 <li key={item}>{item}</li>
                             ))}
                         </ul>
                     </section>
-                    <hr/>
+                    <hr />
+                    <section className="section">
+                        <form className="form" id="addItemForm">
+                            <label className="has-text-weight-bold">Task:</label>
+                            <input type="text" className="input" id="addInput" placeholder="Something that needs ot be done..." />
+                            <br />
+                            <br />
+                            <button className="button is-info" onClick={this.addItem}>Add Item</button>
+                        </form>
+                    </section>
                 </div>
             </div>
         )
